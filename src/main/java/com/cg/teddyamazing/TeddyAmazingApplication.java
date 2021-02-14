9bdb0f02c96ee5cf1e313a081b5ce1910aafe514
thucuc01 <70868459+thucuc01@users.noreply.github.com>
2021-02-14 21:59:03 +0700

    controller

--- src/main/java/com/cg/teddyamazing/TeddyAmazingApplication.java
+++ src/main/java/com/cg/teddyamazing/TeddyAmazingApplication.java
@@ -1,8 +1,15 @@
 package com.cg.teddyamazing;
 
 
+import com.cg.teddyamazing.service.product.CategoryService;
+import com.cg.teddyamazing.service.product.ProductService;
+import com.cg.teddyamazing.service.product.SizeService;
+import com.cg.teddyamazing.service.product.impl.CategoryServiceImpl;
+import com.cg.teddyamazing.service.product.impl.ProductServiceImpl;
+import com.cg.teddyamazing.service.product.impl.SizeServiceImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TeddyAmazingApplication {
@@ -10,19 +17,19 @@ public class TeddyAmazingApplication {
     public static void main(String[] args) {
         SpringApplication.run(TeddyAmazingApplication.class, args);
     }
-//    @Bean
-//    public ProductService productService(){
-//        return new ProductServiceImpl();
-//    }
-//
-//    @Bean
-//    public CategoryService categoryService(){
-//        return new CategoryServiceImpl();
-//    }
-//
-//    @Bean
-//    public SizeService sizeService(){
-//        return new SizeServiceImpl();
-//    }
+    @Bean
+    public ProductService productService(){
+        return new ProductServiceImpl();
+    }
+
+    @Bean
+    public CategoryService categoryService(){
+        return new CategoryServiceImpl();
+    }
+
+    @Bean
+    public SizeService sizeService(){
+        return new SizeServiceImpl();
+    }
 
 }
