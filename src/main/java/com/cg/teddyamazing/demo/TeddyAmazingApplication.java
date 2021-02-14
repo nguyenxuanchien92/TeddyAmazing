02ad818a7647394a355c630933f949be72b37a39
nguyenxuanchien92 <nguyenxuanchien92@gmail.com>
2021-02-14 20:48:43 +0700

    nothing

---
+++ src/main/java/com/cg/teddyamazing/demo/TeddyAmazingApplication.java
@@ -0,0 +1,13 @@
+package com.cg.teddyamazing.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeddyAmazingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeddyAmazingApplication.class, args);
+    }
+
+}
