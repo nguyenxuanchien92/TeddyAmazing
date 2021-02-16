6c6d5f9dfd82b3308a92edd05e94a0b3bea2977a
thucuc01 <70868459+thucuc01@users.noreply.github.com>
2021-02-16 13:12:51 +0700

    update uploadfile + validator

--- src/main/java/com/cg/teddyamazing/controller/product/ProductController.java
+++ src/main/java/com/cg/teddyamazing/controller/product/ProductController.java
@@ -3,21 +3,27 @@ package com.cg.teddyamazing.controller.product;
 
 import com.cg.teddyamazing.model.product.Category;
 import com.cg.teddyamazing.model.product.Product;
+import com.cg.teddyamazing.model.product.ProductForm;
 import com.cg.teddyamazing.model.product.Size;
 import com.cg.teddyamazing.service.product.CategoryService;
 import com.cg.teddyamazing.service.product.ProductService;
 import com.cg.teddyamazing.service.product.SizeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 
 import org.springframework.data.domain.Pageable;
-
 import org.springframework.stereotype.Controller;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.validation.BindingResult;
+
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
-
-
+import javax.validation.Valid;
+import java.io.File;
+import java.io.IOException;
 import java.util.Optional;
 
 @Controller
@@ -42,10 +48,6 @@ public class ProductController {
     }
 
 
-    @GetMapping("/")
-    public String showIndex(){
-        return "index";
-    }
 
     @GetMapping("/home")
     public ModelAndView showHome(@RequestParam("s") Optional<String> s, Pageable pageable){
@@ -75,13 +77,50 @@ public class ProductController {
         return "redirect:/home";
     }
 
+    @Value("${upload.path}")
+    private String fileUpload;
+
+
     @PostMapping("/create-product")
-    public ModelAndView createProduct(@ModelAttribute("product") Product product){
-        productService.save(product);
-        System.out.println(product.getId());
-        ModelAndView modelAndView = new ModelAndView("product/add");
+    public ModelAndView createProduct(@Valid @ModelAttribute("product") ProductForm product, BindingResult bindingResult,Pageable pageable){
+        ModelAndView modelAndView;
+        Page<Product> products=productService.findAll(pageable);
+        for (Product p :
+                products) {
+            System.out.println(p.getId());
+            if (p.getId().equals(product.getId())){
+                modelAndView = new ModelAndView("product/add");
+                modelAndView.addObject("product", new Product());
+                modelAndView.addObject("message", "ID da ton tai");
+                return modelAndView;
+            }
+        }
+
+        if(bindingResult.hasFieldErrors()){
+            modelAndView=new ModelAndView("product/add");
+            return modelAndView;
+        }
+        Product product1 = new Product.ProductBuilder(product.getName())
+                .desv(product.getDesv()).build();
+        MultipartFile multipartFile = product.getImg();
+        String fileName = multipartFile.getOriginalFilename();
+        try {
+            FileCopyUtils.copy(product.getImg().getBytes(), new File(this.fileUpload + fileName));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        product1.setId(product.getId());
+        product1.setName(product.getName());
+        product1.setPrice(product.getPrice());
+        product1.setDesv(product.getDesv());
+        product1.setCategory(product.getCategory());
+        product1.setSize(product.getSize());
+        product1.setImg(fileName);
+        productService.save(product1);
+        modelAndView = new ModelAndView("product/add");
         modelAndView.addObject("product", new Product());
         modelAndView.addObject("message", "New product created successfully");
+
         return modelAndView;
     }
 
@@ -98,11 +137,36 @@ public class ProductController {
             return modelAndView;
         }
     }
+
     @PostMapping("/save-product")
-    public ModelAndView editProduct (@ModelAttribute("product") Product product){
-        productService.save(product);
-        ModelAndView modelAndView = new ModelAndView("product/edit");
-        modelAndView.addObject("product", product);
+    public ModelAndView editProduct (@Valid @ModelAttribute("product") ProductForm product,BindingResult bindingResult){
+
+        ModelAndView modelAndView;
+        if(bindingResult.hasFieldErrors()){
+            modelAndView=new ModelAndView("product/edit");
+            return modelAndView;
+        }
+        Product product1 = new Product.ProductBuilder(product.getName())
+                .desv(product.getDesv()).build();
+
+        MultipartFile multipartFile = product.getImg();
+        String fileName = multipartFile.getOriginalFilename();
+        try {
+            FileCopyUtils.copy(product.getImg().getBytes(), new File(this.fileUpload + fileName));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        product1.setId(product.getId());
+        product1.setName(product.getName());
+        product1.setPrice(product.getPrice());
+        product1.setDesv(product.getDesv());
+        product1.setCategory(product.getCategory());
+        product1.setSize(product.getSize());
+        product1.setImg(fileName);
+        productService.save(product1);
+
+        modelAndView = new ModelAndView("product/edit");
+        modelAndView.addObject("product", new Product());
         modelAndView.addObject("message", "Product updated successfully");
         return modelAndView;
     }
